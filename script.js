7032bcc15766e566093408fa7d0d8c0e115bd8d8
WebDev218 <webdev218@gmail.com>
2017-01-06 14:13:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,9 @@ function openModal(largeImage) {
 	image.setAttribute("src", largeImage);
 	image.setAttribute("id", "modalImage");
 
-
 	// galleryIndex = largeImage.charAt(7);
 	// console.log(largeImage);	
 	// console.log(galleryIndex);	
-
 
 	modalContainer.style.display = "block";
 }
@@ -129,7 +127,6 @@ window.addEventListener("resize", sizeModal);
 
 /* ==== Temporary code ==== */
 
-
 // var galleryLength = 4;
 // var galleryIndex;
 
@@ -162,8 +159,3 @@ window.addEventListener("resize", sizeModal);
 // 	openModal(nextImage);	
 // }
 
-	console.log(galleryIndex);
-	
-	nextImage = "images/" + galleryIndex + "_large.png";
-	openModal(nextImage);		
-}
